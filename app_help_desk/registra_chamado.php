715b66bab8d25e59912dba92af564abd7164d7bb
Kaio Dantas Mancini <113388395+kaio-d@users.noreply.github.com>
2023-03-07 21:10:03 -0300

    Initial Commit

---
+++ app_help_desk/registra_chamado.php
@@ -0,0 +1,16 @@
+<?php
+    session_start();
+
+    $titulo = str_replace("#", "-",$_POST["titulo"]);
+    $categoria = str_replace("#", "-",$_POST["categoria"]);
+    $descricao = str_replace("#", "-",$_POST["descricao"]);
+
+    $texto = $_SESSION["id"] . "#" . $titulo . "#" . $categoria . "#" . $descricao . PHP_EOL;
+
+    $arquivo = fopen("../../app_help_desk/arquivo.hd","a");
+    
+    fwrite($arquivo, $texto);
+    fclose($arquivo);
+    
+    header("location: abrir_chamado.php");
+?>
